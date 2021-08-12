a3ff5fe987266312fed7b4be7393139aee9bf5be
Nicole Galek <nicole@recesscreative.com>
2021-08-12 00:46:24 -0400

    change js ref

--- js/_animations.js
+++ js/_animations.js
@@ -1,6 +1,6 @@
-import slideUp from '/horseladynicole.github.io/transitions/slideUp.js'
-import once from '/horseladynicole.github.io/transitions/once.js'
-import filters from '/horseladynicole.github.io/transitions/filters.js'
+import slideUp from '/js/transitions/slideUp.js'
+import once from '/js/transitions/once.js'
+import filters from '/js/transitions/filters.js'
 
 const { barba, barbaRouter: router} = window
 
